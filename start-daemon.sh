6bab4c1c4218649efd58f73e0b1a87a7e10cc220
Martín D'Agostino <maj.dagostino@gmail.com>
2023-12-30 21:06:46 +0100

    bash script for deamon

--- start-daemon.sh
+++ start-daemon.sh
@@ -2,8 +2,8 @@
 
 ssh-add ~/.ssh/id_ed25519
 
-while [ -z "$SSH_AUTH_SOCK" ]; do
-    sleep 1
-done
+# while [ -z "$SSH_AUTH_SOCK" ]; do
+#     sleep 1
+# done
 
 /Users/margostino/workspace/babel-agent/babel-agent --config /Users/margostino/.babel/babel.toml
