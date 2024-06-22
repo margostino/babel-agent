05f7b6632bb7abe91dbaea7e17d4eaa450ac4154
margostino <maj.dagostino@gmail.com>
2024-06-22 19:32:42 +0200

    git ignore

---
+++ cmd/babel-agent/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/margostino/babel-agent/internal/agent"
+	"github.com/margostino/babel-agent/internal/config"
+)
+
+func main() {
+	log.SetOutput(os.Stdout)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGHUP)
+
+	c := &config.Config{}
+	c.Init(os.Args)
+
+	defer func() {
+		signal.Stop(signalChan)
+		cancel()
+	}()
+
+	go func() {
+		for {
+			select {
+			case s := <-signalChan:
+				switch s {
+				case syscall.SIGHUP:
+					c.Init(os.Args)
+				case os.Interrupt:
+					cancel()
+					os.Exit(1)
+				}
+			case <-ctx.Done():
+				log.Printf("Done.")
+				os.Exit(1)
+			}
+		}
+	}()
+
+	if err := run(ctx, c, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, c *config.Config, stdout io.Writer) error {
+	agent := agent.NewAgent(c)
+	return agent.Run(ctx)
+}
